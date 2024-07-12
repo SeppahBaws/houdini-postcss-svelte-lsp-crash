fc7ef0dd75be324e93a4ab26b4efd9d521cc3b68
Seppe Dekeyser <seppe@cloudclass.be>
2024-07-12 11:35:04 +0200

    Setup tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="flex h-dvh w-dvw flex-col items-center justify-center">
+	<h1 class="text-4xl font-bold">Welcome to SvelteKit</h1>
+	<p class="my-custom-class">
+		Visit <a
+			href="https://kit.svelte.dev"
+			class="font-bold text-orange-600 hover:text-orange-500 hover:underline">kit.svelte.dev</a
+		> to read the documentation
+	</p>
+</div>
+
+<style>
+	.my-custom-class {
+		margin-top: 2rem;
+	}
+</style>
